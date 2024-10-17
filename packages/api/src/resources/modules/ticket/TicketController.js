96e7f8d46fa46add7924478d4ceed03dc26e4b78
2Wesley2 <wesley.fer.almeida98@gmail.com>
2024-10-17 10:33:24 -0300

    refactor

--- packages/api/src/resources/modules/ticket/TicketController.js
+++ packages/api/src/resources/modules/ticket/TicketController.js
@@ -1,27 +1,8 @@
-import { Router } from 'express';
-import config from '../../../config/index.js';
+import Controller from '../../core/Controller.js';
 
-class TicketController {
+class TicketController extends Controller {
   constructor(service) {
-    this.service = service;
-    this.router = Router();
-    this.initializeRoutes();
-  }
-
-  initializeRoutes() {
-    this.router.get('/:id', this.findById.bind(this));
-  }
-
-  async findById(req, res, next) {
-    try {
-      const { id } = req.params;
-      const ticket = await this.service.findById(id);
-      config.logger.info('Controlador: Ticket encontrado', { id });
-      res.status(200).json(ticket);
-    } catch (error) {
-      config.logger.error('Controlador: Erro ao buscar ticket', { id, error });
-      next(error);
-    }
+    super(service);
   }
 }
 
