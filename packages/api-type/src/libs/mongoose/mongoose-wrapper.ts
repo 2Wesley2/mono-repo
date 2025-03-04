7320a3e0d162fc1f5b4574f64e580a3b363174a6
2Wesley2 <wesley.fer.almeida98@gmail.com>
2025-03-04 20:11:21 -0300

    refactor mongoose wrapper

--- packages/api-type/src/libs/mongoose/mongoose-wrapper.ts
+++ packages/api-type/src/libs/mongoose/mongoose-wrapper.ts
@@ -1,51 +1,40 @@
 import mongoose, {
   Schema,
+  SchemaDefinition,
   Model,
   Document as MongooseDocument,
   ConnectOptions,
-  PreMiddlewareFunction,
-  PostMiddlewareFunction,
-  SchemaOptions,
 } from "mongoose";
 import config from "../../config/index";
-import type {
-  Middleware,
-  AddMiddleware,
-  RegisterModel,
-  ConfigSchema,
-  SubSchema,
+import {
+  RegisterDocumentConfigurator,
+  RegisterMiddlewaresConfigurator,
 } from "./type-mongoose-wrapper";
 
+import type { MiddlewareConfig, options } from "./type-mongoose-wrapper";
 export class MongooseWrapper {
-  static addMiddleware: AddMiddleware["addMiddleware"] = (
-    schema,
-    type,
-    event,
-    fn,
-  ) => {
-    if (!schema || !(schema instanceof Schema)) {
-      throw new Error(
-        "O primeiro argumento deve ser um schema Mongoose válido.",
-      );
-    }
-    if (!["pre", "post"].includes(type)) {
-      throw new Error(
-        `O tipo de middleware "${type}" é inválido. Use "pre" ou "post".`,
-      );
-    }
-    if (typeof event !== "string" || typeof fn !== "function") {
-      throw new Error(
-        'Os argumentos "event" e "fn" devem ser uma string e uma função, respectivamente.',
-      );
-    }
-    if (type === "pre") {
-      schema.pre(event, fn as PreMiddlewareFunction<any>);
-    } else {
-      schema.post(event, fn as PostMiddlewareFunction<any>);
-    }
-  };
+  static addMiddleware(
+    schema: Schema,
+    middlewares: MiddlewareConfig[],
+  ): RegisterMiddlewaresConfigurator<Schema> {
+    return new RegisterMiddlewaresConfigurator(schema, middlewares);
+  }
 
-  static async connect(dbName: string = config.dbName): Promise<void> {
+  static registerDocument(
+    schema: SchemaDefinition,
+    modelName: string,
+    options: options,
+    RegisterMiddlewares: MiddlewareConfig[],
+  ): Model<MongooseDocument> {
+    return new RegisterDocumentConfigurator(
+      schema,
+      modelName,
+      options,
+      RegisterMiddlewares,
+    ).model;
+  }
+
+  static connect = async (dbName = config.dbName) => {
     if (!dbName) {
       throw new Error("O nome do banco de dados não pode ser undefined.");
     }
@@ -93,127 +82,14 @@ export class MongooseWrapper {
       console.error(`Falha ao conectar ao banco ${dbName}:`, error.message);
       throw error;
     }
-  }
+  };
 
-  static async disconnect(): Promise<void> {
+  static disconnect = async () => {
     try {
       await mongoose.connection.close();
       console.log("Conexão com o banco de dados encerrada.");
     } catch (error: any) {
       console.error("Erro ao encerrar a conexão:", error);
     }
-  }
-
-  static configSchema: ConfigSchema["configSchema"] = ({
-    schema,
-    options = {},
-    middlewares = [],
-  }) => {
-    const newSchema: Schema = new Schema(schema, options);
-    middlewares.forEach(({ type, event, fn }) => {
-      this.addMiddleware(newSchema, type, event, fn);
-    });
-    return newSchema;
   };
-
-  static subSchema: SubSchema["subSchema"] = (
-    definition,
-    options = {},
-    middlewares = [],
-  ) => {
-    const opts = { _id: false, ...options };
-    const subSchema: Schema = new Schema(definition, opts);
-    middlewares.forEach(({ type, event, fn }) => {
-      this.addMiddleware(subSchema, type, event, fn);
-    });
-    return subSchema;
-  };
-
-  static registerModel: RegisterModel["registerModel"] = <
-    T extends MongooseDocument,
-  >({
-    schema,
-    modelName,
-    options = {},
-    middlewares = [],
-  }: {
-    schema: Record<string, any>;
-    modelName: string;
-    options?: SchemaOptions;
-    middlewares?: Middleware[];
-  }): Model<MongooseDocument & T> => {
-    if (!schema || typeof schema !== "object") {
-      throw new Error("Schema inválido ou não fornecido.");
-    }
-
-    if (!modelName || typeof modelName !== "string") {
-      throw new Error(
-        `O nome do modelo deve ser uma string válida. Recebido: ${modelName}`,
-      );
-    }
-
-    if (mongoose.models[modelName]) {
-      console.log(`Modelo "${modelName}" já está registrado.`);
-      return mongoose.models[modelName] as Model<T>;
-    }
-    const newOptions = { timestamps: true, ...options };
-    const newSchema: Schema = this.configSchema({
-      schema,
-      options: newOptions,
-      middlewares,
-    });
-    const model: Model<T> = mongoose.model<T>(modelName, newSchema);
-    return model;
-  };
-
-  static get ObjectIdTypeMongoose() {
-    return mongoose.Schema.Types.ObjectId;
-  }
-
-  static get TypesMongoose() {
-    return mongoose.Schema.Types;
-  }
-
-  static isValidObjectId(id: string): boolean {
-    return mongoose.Types.ObjectId.isValid(id);
-  }
-
-  static toObjectId(...values: (string | number)[]): mongoose.Types.ObjectId[] {
-    return values.map((value) => {
-      if (mongoose.Types.ObjectId.isValid(value)) {
-        return new mongoose.Types.ObjectId(String(value));
-      }
-      throw new Error(`O valor fornecido (${value}) não é um ObjectId válido.`);
-    });
-  }
-
-  static async dropDatabase(...dbNames: string[]): Promise<void> {
-    if (mongoose.connection.readyState !== 1) {
-      throw new Error("Não há conexão ativa com o MongoDB.");
-    }
-
-    try {
-      for (const dbName of dbNames) {
-        if (typeof dbName !== "string" || !dbName) {
-          throw new Error(
-            "O nome do banco de dados deve ser uma string válida.",
-          );
-        }
-        const dbConnection = mongoose.connection.useDb(dbName);
-        await dbConnection.dropDatabase();
-        console.log(`Banco de dados "${dbName}" excluído com sucesso.`);
-      }
-    } catch (error: any) {
-      console.error(`Erro ao excluir o banco de dados:`, error);
-      throw error;
-    }
-  }
-
-  static get getTypes() {
-    return mongoose.Schema.Types;
-  }
-
-  static get getObjectIdType() {
-    return mongoose.Schema.Types.ObjectId;
-  }
 }
--- packages/api-type/src/libs/mongoose/type-mongoose-wrapper.ts
+++ packages/api-type/src/libs/mongoose/type-mongoose-wrapper.ts
@@ -1,60 +1,96 @@
-import {
+import mongoose, {
   Schema,
-  Model,
-  Document as MongooseDocument,
-  PreMiddlewareFunction,
-  PostMiddlewareFunction,
+  SchemaDefinition,
   SchemaOptions,
-  MongooseDocumentMiddleware,
-  MongooseQueryMiddleware,
+  Model,
+  Document,
+  models,
 } from "mongoose";
 
-export interface Middleware {
-  type: "pre" | "post";
-  event: MongooseDocumentMiddleware | MongooseQueryMiddleware;
-  fn: PreMiddlewareFunction<any> | PostMiddlewareFunction<any>;
+export type Default = {};
+export type NewDocMongoose<TDoc extends Document = Document> = Model<TDoc>;
+export type options = SchemaOptions<any, any, any, any, Default>;
+export type MiddlewareConfig = {
+  readonly method: "pre" | "post";
+  readonly hookEvent: RegExp;
+  readonly fn: (...args: any[]) => void | Promise<void>;
+};
+
+export interface RegisterMiddlewares {}
+export class RegisterMiddlewaresConfigurator<T extends Schema>
+  implements RegisterMiddlewares
+{
+  constructor(
+    public readonly schema: T,
+    private readonly middlewares: MiddlewareConfig[],
+  ) {
+    this.registerMiddlewares();
+  }
+
+  private registerMiddlewares(): Schema {
+    this.middlewares.forEach((mw) => {
+      if (mw.method === "pre") {
+        this.schema.pre(mw.hookEvent, mw.fn);
+      } else if (mw.method === "post") {
+        this.schema.post(mw.hookEvent, mw.fn);
+      }
+    });
+    return this.schema;
+  }
 }
 
-export interface AddMiddleware {
-  addMiddleware(
-    schema: Schema,
-    type: "pre" | "post",
-    event: MongooseDocumentMiddleware | MongooseQueryMiddleware,
-    fn: PreMiddlewareFunction<any> | PostMiddlewareFunction<any>,
-  ): void;
+export interface RegisterModel {}
+export class RegisterModelConfigurator<T extends Document>
+  implements RegisterModel
+{
+  public model: Model<T>;
+
+  constructor(
+    private readonly modelName: string,
+    private readonly schema: Schema,
+  ) {
+    this.model = this.registerModel();
+  }
+
+  private registerModel(): Model<T> {
+    if (models[this.modelName]) {
+      console.log(`Modelo "${this.modelName}" já está registrado.`);
+      return models[this.modelName];
+    }
+    return mongoose.model<T>(this.modelName, this.schema);
+  }
 }
 
-export interface ConfigSchema {
-  configSchema(params: {
-    schema: Record<string, any>;
-    options?: SchemaOptions;
-    middlewares?: Middleware[];
-  }): Schema;
+export interface RegisterDocumentParams<TDoc extends Document = Document> {
+  readonly schema: SchemaDefinition;
+  readonly modelName: string;
+  readonly options?: options;
+  readonly middlewares?: MiddlewareConfig[];
+
+  readonly __docType?: TDoc;
 }
 
-export interface SubSchemaParams {
-  definition: object;
-  options?: object;
-  middlewares?: Middleware[];
-}
+export interface RegisterDocument {}
+export class RegisterDocumentConfigurator<
+  TDoc extends Document,
+  T extends RegisterDocumentParams<TDoc>,
+> implements RegisterDocument
+{
+  public model: NewDocMongoose<TDoc>;
+  constructor(
+    private readonly schema: T["schema"],
+    private readonly modelName: T["modelName"],
+    private readonly options?: T["options"],
+    private readonly middlewares?: T["middlewares"],
+  ) {
+    this.model = this.registerDocument();
+  }
 
-export interface SubSchema {
-  subSchema(
-    definition: SubSchemaParams["definition"],
-    options?: SubSchemaParams["options"],
-    middlewares?: SubSchemaParams["middlewares"],
-  ): Schema;
-}
-
-export interface RegisterModelParams {
-  schema: Record<string, any>;
-  modelName: string;
-  options?: SchemaOptions;
-  middlewares?: Middleware[];
-}
-
-export interface RegisterModel {
-  registerModel<T extends MongooseDocument>(
-    params: RegisterModelParams,
-  ): Model<MongooseDocument & T>;
+  private registerDocument(): NewDocMongoose<TDoc> {
+    const schema = new Schema(this.schema, this.options);
+    if (this.middlewares) {
+      new RegisterMiddlewaresConfigurator(schema, this.middlewares);
+    }
+    return new RegisterModelConfigurator<TDoc>(this.modelName, schema).model;
+  }
 }
